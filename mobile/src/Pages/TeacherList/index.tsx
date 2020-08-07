51a117fb3708be3db5e2d7c9552914786c47b106
Vinicius Stephanto <50119865+stephanto-dev@users.noreply.github.com>
2020-08-07 16:48:57 -0300

    Aula 05 - Integração com a API no mobile

--- mobile/src/Pages/TeacherList/index.tsx
+++ mobile/src/Pages/TeacherList/index.tsx
@@ -1,13 +1,131 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../../services/api';
+
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacerItem';
+
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
+import { useFocusEffect } from '@react-navigation/native';
+
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+    const [favorites, setFavorites] = useState<number[]>([])
+    const [isFilterVisible, serIsFilterVisible] = useState(false);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response)
+                const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => { return teacher.id })
+                setFavorites(favoritedTeachersIds);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
+    function handleToogleFiltersVisible() {
+        serIsFilterVisible(!isFilterVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        loadFavorites();
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis" />
+
+            <PageHeader title="Proffys disponíveis"
+                headerRight={(
+                    <BorderlessButton onPress={handleToogleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#FFF" />
+                    </BorderlessButton>
+                )}>
+                {isFilterVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
+                            placeholderTextColor="#c1bccc"
+                            placeholder="Qual a matéria?"
+                        />
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Dia da semana</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)}
+                                    placeholderTextColor="#c1bccc"
+                                    placeholder="Qual o dia?"
+                                />
+                            </View>
+
+
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
+                                    placeholderTextColor="#c1bccc"
+                                    placeholder="Qual o horário?"
+                                />
+                            </View>
+
+                        </View>
+
+                        <RectButton style={styles.submitButton} onPress={handleFiltersSubmit}>
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
+                    </View>
+                )}
+            </PageHeader>
+            <ScrollView
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+                {teachers.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={favorites.includes(teacher.id)}
+                        />
+                    )
+                })}
+            </ScrollView>
+
+
         </View>
     );
 }
